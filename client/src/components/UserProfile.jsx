89574c9469517afe383856f98ad3e475c0ae41fa
Iqbal Sahrizal <54559728+iqbal2906@users.noreply.github.com>
2022-01-25 18:53:33 +0700

    create userProfile page

--- client/src/components/UserProfile.jsx
+++ client/src/components/UserProfile.jsx
@@ -20,6 +20,11 @@ const UserProfile = () => {
   const navigate = useNavigate();
   const { userId } = useParams();
 
+  const activeBtnStyles =
+    "bg-red-500 text-white font-bold p-2 rounded-full w-20 outline-none";
+  const notActiveBtnStyles =
+    "bg-primary mr-4 text-black font-bold p-2 rounded-full w-20 outline-none";
+
   const randomImage =
     "https://source.unsplash.com/1600x900/?nature,photography,technology";
 
@@ -31,11 +36,25 @@ const UserProfile = () => {
     });
   }, [userId]);
 
-  const logout = () => {
-    localStorage.clear()
+  useEffect(() => {
+    if (text === "Created") {
+      const createdPinsQuery = userCreatedPinsQuery(userId);
+      client.fetch(createdPinsQuery).then((data) => {
+        setPins(data);
+      });
+    } else {
+      const savedPinsQuery = userSavedPinsQuery(userId);
+      client.fetch(savedPinsQuery).then((data) => {
+        setPins(data);
+      });
+    }
+  }, [text, userId]);
 
-    navigate('/login')
-  }
+  const logout = () => {
+    localStorage.clear();
+
+    navigate("/login");
+  };
 
   if (!user) {
     return <Spinner message="Loading profile..." />;
@@ -56,27 +75,64 @@ const UserProfile = () => {
               src={user.image}
               alt="user-pic"
             />
-            <h1 className="font-bold text-3xl text-center mt-3">{user.userName}</h1>
+            <h1 className="font-bold text-3xl text-center mt-3">
+              {user.userName}
+            </h1>
             <div className="absolute top-0 z-1 right-0 p-2">
               {userId === user._id && (
-                <GoogleLogout 
-                clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}
-                render={(renderProps) => (
-                    <button 
-                        type="button"
-                        className="bg-white p-2 rounded-full cursor-pointer outline-none shadow-md"
-                        onClick={renderProps.onClick}
-                        disabled={renderProps.disabled}
+                <GoogleLogout
+                  clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}
+                  render={(renderProps) => (
+                    <button
+                      type="button"
+                      className="bg-white p-2 rounded-full cursor-pointer outline-none shadow-md"
+                      onClick={renderProps.onClick}
+                      disabled={renderProps.disabled}
                     >
-                        <AiOutlineLogout color="red" fontSize={21}/>
+                      <AiOutlineLogout color="red" fontSize={21} />
                     </button>
-                )}
-                onLogoutSuccess={logout}
-                cookiePolicy="single_host_origin"
-            />
+                  )}
+                  onLogoutSuccess={logout}
+                  cookiePolicy="single_host_origin"
+                />
               )}
             </div>
           </div>
+          <div className="text-center mb-7">
+            <button
+              type="button"
+              onClick={(e) => {
+                setText(e.target.textContent);
+                setActiveBtn("created");
+              }}
+              className={`${
+                activeBtn === "created" ? activeBtnStyles : notActiveBtnStyles
+              }`}
+            >
+              Created
+            </button>
+            <button
+              type="button"
+              onClick={(e) => {
+                setText(e.target.textContent);
+                setActiveBtn("saved");
+              }}
+              className={`${
+                activeBtn === "saved" ? activeBtnStyles : notActiveBtnStyles
+              }`}
+            >
+              Saved
+            </button>
+          </div>
+          {pins?.length ? (
+            <div className="px-2">
+              <MasonryLayout pins={pins} />
+            </div>
+          ) : (
+            <div className="flex justify-center font-bold items-center w-full text-xl mt-2">
+              No Pins Found
+            </div>
+          )}
         </div>
       </div>
     </div>
